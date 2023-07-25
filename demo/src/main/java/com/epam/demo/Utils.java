c4bec25dd09735f29783c2097071069f13f33c39
victor diaz <vdmpeniel@yahoo.es>
2023-07-25 19:20:45 -0400

    Added all required changes.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Utils.isAllPositiveNumbers(List.of("1", "-3", "6", "8")));
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        return Integer.valueOf(str) < 0 ? false : true;
     }
 }
