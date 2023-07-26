b81b42405d95a9efb856e7f664505e98de95a4e8
victor diaz <vdmpeniel@yahoo.es>
2023-07-25 20:37:03 -0400

    Added support to reject leading zeroes.

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        String regex = "^0+[0-9].*";
+        if(str.matches(regex)){ return false; }
+
         try {
             return Double.parseDouble(str) > 0;
         } catch (Exception e){
@@ -12,6 +15,6 @@ public class StringUtils {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPositiveNumber("0.5"));
+        System.out.println(isPositiveNumber("088"));
     }
 }
